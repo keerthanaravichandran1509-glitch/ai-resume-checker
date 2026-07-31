c66c1322ba42b998c5dcafa3f532cca3dba1b8f4
Keerthana Ravichandran <keerthanaravichandran1509@gmail.com>
2026-07-31 04:53:32 +0000

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>AI Resume Checker v2</h1>
+          <h1>AI Resume Checker v3
+          </h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
